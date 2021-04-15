c9817bdb52ddc28a485c87bb0d7cdc939deb77b0
Jonathan Laroche <larochej7007@gmail.com>
2021-04-15 09:58:10 -0400

    Cmon work, need to build first?

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var app = express();
 var port = process.env.PORT || 8080
 
 app.use(express.static(__dirname + "/dist"));
-app.use("/assets", express.static(__dirname + '/assets'));
+app.use("/assets", express.static(__dirname + '/dist'));
+app.use("/assets/data", express.static(__dirname + '/dist/data'));
 
 app.get("/", function(req, res) {
     res.render("index");
