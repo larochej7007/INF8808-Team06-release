a869608794212e6f03048dcc0bf92ead6edcd2e3
Jonathan Laroche <larochej7007@gmail.com>
2021-04-15 02:24:01 -0400

    Should publish

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var app = express();
 
 var port = process.env.PORT || 8080
 
-app.use(express.static(__dirname + 'dist/'));
+app.use(express.static(__dirname + "/dist"));
+app.use("/assets", express.static(__dirname + '/assets'));
 
 app.get("/", function(req, res) {
     res.render("index.js");
