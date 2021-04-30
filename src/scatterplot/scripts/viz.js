1e4df4085c242f2bbc8124042e07301e1d415387
LSNoor <lucnoor@gmail.com>
2021-04-30 00:51:09 -0400

    44

--- src/scatterplot/scripts/viz.js
+++ src/scatterplot/scripts/viz.js
@@ -88,7 +88,7 @@ export function moveCircles (xScale, yScale, transitionDuration) {
  */
 export function setTitleText (year) {
  
-  var xtranslate = d3.select('#scatterplot').node().getBoundingClientRect().width - 250
+  var xtranslate = d3.select('#scatterplot').node().getBoundingClientRect().width - 350
   console.log(xtranslate)
 
   d3.select('#title-scatterplot')
