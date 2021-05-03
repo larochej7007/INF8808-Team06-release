754465c08b8bc00b47d1441b1e9f663e60c522b4
LSNoor <lucnoor@gmail.com>
2021-05-03 01:10:25 -0400

    fdsdfssdf

--- src/scatterplot/scripts/helper.js
+++ src/scatterplot/scripts/helper.js
@@ -122,7 +122,7 @@ export function drawButton1 (g, year) {
   button.append('rect')
     .attr('width', 150)
     .attr('height', 30)
-    .attr('fill', '#898d89')
+    .attr('fill', '#f4f6f4')
     .on('mouseenter', function () {
       d3.select(this).attr('stroke', '#362023').attr('transform','translate(0,2)')
     })
